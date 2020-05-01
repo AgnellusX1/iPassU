fcdfcc3d46a7ba81f590a52c1c1bc18a5196a333
AgnellusX1 <43168803+AgnellusX1@users.noreply.github.com>
2020-05-01 21:35:01 +0530

    ADDED: SHA1, SHA224, SHA384

--- passCrack.py
+++ passCrack.py
@@ -35,6 +35,76 @@ def fun_md5():
         print("Password not found, try another algo or wordlist")
         print("================================================")
 
+# SHA1
+def fun_sha1():
+    try:
+        pass_file = open(word_list,"r")
+    except:
+        print("File Not Found")
+        quit()
+
+    flag=0
+    counter=0
+    print("Running SHA-1")
+    print("=================")
+    Lines=pass_file.readlines()
+    for word in Lines:
+        enc_word=word.encode('utf-8')
+        digest=hashlib.sha1(enc_word.strip()).hexdigest()
+
+        counter+=1
+        if digest == pass_hash:
+            print("=================")
+            print("Password Found")
+            print(word)
+            print("=================")
+            print("Passwords Checked: "+str(counter))
+            flag=1
+            break
+
+        if flag == 0:
+            print("Please wait....Searching...")
+    if flag==0:
+        print("================================================")
+        print("Password not found, try another algo or wordlist")
+        print("================================================")
+
+# SHA 2 Algorithms
+
+# SHA224
+def fun_sha224():
+    try:
+        pass_file = open(word_list,"r")
+    except:
+        print("File Not Found")
+        quit()
+
+    flag=0
+    counter=0
+    print("Running SHA-224")
+    print("=================")
+    Lines=pass_file.readlines()
+    for word in Lines:
+        enc_word=word.encode('utf-8')
+        digest=hashlib.sha224(enc_word.strip()).hexdigest()
+
+        counter+=1
+        if digest == pass_hash:
+            print("=================")
+            print("Password Found")
+            print(word)
+            print("=================")
+            print("Passwords Checked: "+str(counter))
+            flag=1
+            break
+
+        if flag == 0:
+            print("Please wait....Searching...")
+    if flag==0:
+        print("================================================")
+        print("Password not found, try another algo or wordlist")
+        print("================================================")
+
 # SHA256
 def fun_sha256():
     try:
@@ -51,6 +121,40 @@ def fun_sha256():
     for word in Lines:
         enc_word=word.encode('utf-8')
         digest=hashlib.sha256(enc_word.strip()).hexdigest()
+
+        counter+=1
+        if digest == pass_hash:
+            print("=================")
+            print("Password Found")
+            print(word)
+            print("=================")
+            print("Passwords Checked: "+str(counter))
+            flag=1
+            break
+
+        if flag == 0:
+            print("Please wait....Searching...")
+    if flag==0:
+        print("================================================")
+        print("Password not found, try another algo or wordlist")
+        print("================================================")
+
+# SHA384
+def fun_sha384():
+    try:
+        pass_file = open(word_list,"r")
+    except:
+        print("File Not Found")
+        quit()
+
+    flag=0
+    counter=0
+    print("Running SHA-384")
+    print("=================")
+    Lines=pass_file.readlines()
+    for word in Lines:
+        enc_word=word.encode('utf-8')
+        digest=hashlib.sha384(enc_word.strip()).hexdigest()
 
         counter+=1
         if digest == pass_hash:
@@ -103,6 +207,8 @@ def fun_sha512():
         print("Password not found, try another algo or wordlist")
         print("================================================")
 
+
+# Driver Code
 print("=============================")
 print("========||| iPassU |||========")
 print("A Hash Based password Cracker")
